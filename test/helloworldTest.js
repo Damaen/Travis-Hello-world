e21096ab021fa26d3d9b594d25775f9011632840
loyde.b.bjerke@gmail.com <loyde@Thoth@neon.com>
2021-06-26 12:53:38 -0500

    initial commit

---
+++ test/helloworldTest.js
@@ -0,0 +1,7 @@
+var should = require('should');
+var helloworld = require('../src/helloworld');
+describe('helloworld', function () {
+  it('should equal', function () {
+    helloworld.should.equal('helloworld');
+  });
+});
